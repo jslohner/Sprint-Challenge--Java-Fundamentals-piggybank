50cfda2e296e59b572abb55040b76cb7ab671509
Justin Lohner <lohner4@gmail.com>
2020-06-05 11:12:21 -0600

    Wrote nickel class

---
+++ src/piggybank/Nickel.java
@@ -0,0 +1,25 @@
+package piggybank;
+
+public class Nickel extends AbstractMoney {
+	private String type;
+	private double faceValue;
+
+	public Nickel() {
+		type = "Nickel";
+		faceValue = 0.05;
+	}
+
+	public Nickel(long quantity) {
+		super(quantity);
+		type = "Nickel";
+		faceValue = 0.05;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public double getFaceValue() {
+		return faceValue;
+	}
+}
