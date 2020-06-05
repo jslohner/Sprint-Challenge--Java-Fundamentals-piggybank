cb3eaf803f081e20858949778c92056fdf3f4daf
Justin Lohner <lohner4@gmail.com>
2020-06-05 11:54:29 -0600

    Made getTotalMoney function and formatted print string for total

--- src/piggybank/AbstractMoney.java
+++ src/piggybank/AbstractMoney.java
@@ -30,9 +30,3 @@ abstract class AbstractMoney {
 	// 	return "Total Value of " + type + "s - " + faceValue * quantity;
 	// }
 }
-
-// Each element in the collection contains
-// - The number of coins in this group
-// - The face value of the coins in the group
-// - How to print the total value of this group which is face value * number of coins in the group
-// - When creating a money object, you can optionally give the number of coins being added. If no number is given, the default is 1 coin being added to the piggy bank.
--- src/piggybank/Main.java
+++ src/piggybank/Main.java
@@ -3,13 +3,16 @@ package piggybank;
 import java.util.*;
 
 public class Main {
+	private static double getTotalMoney(List<AbstractMoney> piggyBank) {
+		double total = 0;
+		for (AbstractMoney m : piggyBank) {
+			total += m.getTotal();
+		}
+		return total;
+	}
+
 	private static void workWithPiggyBank() {
 		List<AbstractMoney> piggyBank = new ArrayList<>();
-
-		// piggyBank.add(new Dollar(5));
-		// piggyBank.add(new Dollar(2));
-		// piggyBank.add(new Quarter());
-		// piggyBank.add(new Quarter(3));
 
 		piggyBank.add(new Quarter());
 		piggyBank.add(new Dime());
@@ -22,6 +25,9 @@ public class Main {
 		for (AbstractMoney m : piggyBank) {
 			System.out.println(m);
 		}
+
+		String total = String.format("%.2f", getTotalMoney(piggyBank));
+		System.out.println("The piggy bank holds $" + total);
 	}
 
 	public static void main(String[] args) {
